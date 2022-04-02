13bc84bd7f49f363d780f4c9449e80f10c97f292
Dante Falzone <claire0000@protonmail.com>
2022-04-01 21:16:13 -0500

    did more work

--- assembler/numeric_types.py
+++ assembler/numeric_types.py
@@ -1,11 +1,21 @@
+import ctypes
+
 class I32:
   def __init__(self, value: int):
-    if value > 2147483647:
-      self.value = -2147483648 + (value - 2147483648)
-    elif value < -2147483648:
-      self.value = 2147483647 + (value + 2147483647)
+    if value > 2147483647 or value < -2147483648:
+      raise ValueError(f"unable to store value {value} in a 32-bit signed integer")
     else:
-      self.value = value
+      self.value = ctypes.c_int32(value)
 
-print(str(I32(2147483649).value)) # should print -2147483646
-print(str(I32(-2147483650).value)) # should print 2147483645
+
+class U32:
+  def __init__(self, value: int):
+    if value > 4294967295 or value < 0:
+      raise ValueError(f"unable to store value {value} in a 32-bit unsigned integer")
+    else:
+      self.value = ctypes.c_uint32(value)
+
+
+class F32:
+  def __init__(self, value: float):
+    self.value = ctypes.c_float(value)
